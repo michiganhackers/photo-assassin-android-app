88c35e3d0ccb907fba9c23bc2358c7499f2416fc
Owain Kert <owaink@umich.edu>
2019-02-07 21:20:13 -0500

    Started implementing camera

--- app/src/main/java/org/michiganhackers/photoassassin/CameraHandler.java
+++ app/src/main/java/org/michiganhackers/photoassassin/CameraHandler.java
@@ -1,9 +1,13 @@
 package org.michiganhackers.photoassassin;
 
+import android.content.Context;
 import android.hardware.Camera;
 import android.widget.Toast;
 
 public class CameraHandler {
+    public CameraHandler(Context context) {
+        
+    }
     public static Camera getCameraInstance(){
         Camera c = null;
         try {
