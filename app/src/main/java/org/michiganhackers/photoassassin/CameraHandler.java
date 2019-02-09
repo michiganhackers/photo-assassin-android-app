1c8301bf36073bc51bce9bae2c446d47e3223de8
Owain Kert <owaink@umich.edu>
2019-02-09 16:11:14 -0500

    Started implementing camera

--- app/src/main/java/org/michiganhackers/photoassassin/CameraHandler.java
+++ app/src/main/java/org/michiganhackers/photoassassin/CameraHandler.java
@@ -4,16 +4,23 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.widget.Toast;
 
-public class CameraHandler {
-    public CameraHandler(Context context) {
-        
+class CameraHandler {
+
+    private static Toast errorToast, succToast;
+    CameraHandler(Context context) {
+        errorToast = Toast.makeText(context, "Error Opening Camera", Toast.LENGTH_LONG);
+        succToast = Toast.makeText(context, "Successfully Opened Camera", Toast.LENGTH_LONG);
     }
-    public static Camera getCameraInstance(){
+
+    static Camera getCameraInstance(){
         Camera c = null;
         try {
             c = Camera.open();
-        } catch (Exception e) {
-            Toast toast = Toast.makeText(getApplicationContext(), )
+            succToast.show();
+        } catch (RuntimeException e) {
+
+        } catch (Exception e1) {
+            errorToast.show();
         }
         return c;
     }
