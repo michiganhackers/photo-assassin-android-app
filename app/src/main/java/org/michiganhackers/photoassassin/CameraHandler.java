dcb9cc3a11028d3a4083311d4887eba3d06145ff
ben schondorf <ski31200@aol.com>
2019-02-09 17:35:11 -0500

    Implemented camera and preview

--- app/src/main/java/org/michiganhackers/photoassassin/CameraHandler.java
+++ app/src/main/java/org/michiganhackers/photoassassin/CameraHandler.java
@@ -1,25 +1,52 @@
 package org.michiganhackers.photoassassin;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 class CameraHandler {
+    Activity activity;
+    Camera mCamera;
+    CameraPreview mPreview;
+
+    private static final int CAMERA_PERMISSION_REQUESTS = 42069;
 
     private static Toast errorToast, succToast;
-    CameraHandler(Context context) {
+    CameraHandler(Context context, Activity act) {
         errorToast = Toast.makeText(context, "Error Opening Camera", Toast.LENGTH_LONG);
         succToast = Toast.makeText(context, "Successfully Opened Camera", Toast.LENGTH_LONG);
+        activity = act;
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUESTS);
+        }
+        else{
+            mCamera = getCameraInstance();
+            mCamera.setDisplayOrientation(90);
+            Camera.Parameters params = mCamera.getParameters();
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            mCamera.setParameters(params);
+            mPreview = new CameraPreview(context, mCamera);
+            FrameLayout preview = (FrameLayout) activity.findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+
+        }
     }
 
-    static Camera getCameraInstance(){
+    Camera getCameraInstance(){
         Camera c = null;
+
         try {
             c = Camera.open();
             succToast.show();
-        } catch (RuntimeException e) {
 
-        } catch (Exception e1) {
+        } catch (Exception e) {
             errorToast.show();
         }
         return c;
--- app/src/main/java/org/michiganhackers/photoassassin/MainActivity.java
+++ app/src/main/java/org/michiganhackers/photoassassin/MainActivity.java
@@ -1,16 +1,33 @@
 package org.michiganhackers.photoassassin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
+    private static final int CAMERA_PERMISSION_REQUESTS = 42069;
+    CameraHandler camHan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CameraHandler camHan = new CameraHandler(this);
-        camHan.getCameraInstance();
+        camHan = new CameraHandler(this,this);
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults){
+       if(requestCode == CAMERA_PERMISSION_REQUESTS) {
+           camHan.getCameraInstance();
+
+       }
     }
 }
