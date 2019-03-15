bf83708555a0b12e69354fbc2f35c9a97719ad4d
realdy <realdy@umich.edu>
2019-03-14 20:34:25 -0400

    Added comments

--- app/src/main/java/org/michiganhackers/photoassassin/CameraHandler.java
+++ app/src/main/java/org/michiganhackers/photoassassin/CameraHandler.java
@@ -18,8 +18,12 @@ import androidx.core.content.ContextCompat;
 
 @SuppressWarnings("deprecation")
 class CameraHandler {
+    //Activity deals with what is running in the app
+    //Camera activity,
     private Activity activity;
+    //Deals with the "context" of the activity
     private Context context;
+    //Camera class, deals with the functions for zoom, taking a picture --> camera functionality
     private Camera mCamera;
     private static final String TAG = "CameraHandler";
     //rear camera is 0, front is 1
@@ -28,26 +32,35 @@ class CameraHandler {
 
     private static int numberOfCameras;
     private static Toast errorToast, succToast;
+    //if you have egregious code, it'll create a red squiggly
     @SuppressWarnings("ShowToast")
+
+    //non default constructor
     CameraHandler(Context context, Activity activity) {
 
         errorToast = Toast.makeText(context, "Error Opening Camera", Toast.LENGTH_LONG);
         succToast = Toast.makeText(context, "Successfully Opened Camera", Toast.LENGTH_LONG);
-
+        //this.activity is the activity in the class
         this.activity = activity;
         this.context = context;
 
         numberOfCameras = Camera.getNumberOfCameras();
         Log.v(TAG, "Number of Cameras: " + Integer.toString(numberOfCameras));
         //request permissions if not already granted
+        //If permission not granted, request Permission to ActivityCompat
         if (ContextCompat.checkSelfPermission(activity,
                 Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity,
+                    //using new to create a string in dyanmic memory?
+                    //automatically deletes it at the end because it has garbage collection
                     new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUESTS);
         }
     }
     //returns a Camera set in portrait mode with autofocus mode on
+    //if it fails, a null camera instance returns
     Camera openCamera(int cameraId) {
+        //cameraID is in reference to "rear camera = 0" and "front = 1?"
+        //we use don't use "this" because there is no other variable with the name "mCamera"
         mCamera = null;
         try {
             mCamera = Camera.open(cameraId);
@@ -61,7 +74,8 @@ class CameraHandler {
             mCamera.setParameters(params);
 
             succToast.show();
-
+            //if there's any error or exception that occurs in try, the catch is executed and the
+            // error message shows
         } catch (Exception e) {
             errorToast.show();
         }
@@ -80,9 +94,11 @@ class CameraHandler {
                 openedCamera = 0;
             } else {
                 mCamera = openCamera(0);
+                //log e deals with printing to the Logcat
+                //not viewable by the user, but can be used for error checkign
                 Log.e(TAG,  "Something went wrong, defaulting to camera 0");
             }
-
+            //if exception e is to catch any possible error that occurs
         } catch (Exception e) {
             Log.e(TAG, "failed to switch camera, probably only one camera");
         }
@@ -91,6 +107,7 @@ class CameraHandler {
     }
 
     void showCameraPreview(Camera camera) {
+        //CameraPreview deals with the live video that's shown when the camera app is open
         CameraPreview mPreview = new CameraPreview(activity, camera);
         FrameLayout preview = activity.findViewById(R.id.camera_preview);
         preview.addView(mPreview);
