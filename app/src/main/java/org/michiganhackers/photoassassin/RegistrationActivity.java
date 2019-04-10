6cf8e37d4f137c2452394ef2435251867ea7d876
Vincent Nagel <vnagel@umich.edu>
2019-04-10 13:41:01 -0400

    Sign in with google seems to be working
    Might not be safe to have web client id in strings.
    Added coordinator layout for snackbar.
    Removed firbase cor lib from gradle because it seems to not be necessary.

--- app/src/main/java/org/michiganhackers/photoassassin/RegistrationActivity.java
+++ app/src/main/java/org/michiganhackers/photoassassin/RegistrationActivity.java
@@ -2,27 +2,40 @@ package org.michiganhackers.photoassassin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private TextInputEditText emailEditText, passwordEditText;
     private TextInputLayout emailTextInputLayout, passwordTextInputLayout;
+    private CoordinatorLayout coordinatorLayout;
     private FirebaseAuth auth;
+    private GoogleSignInClient googleSignInClient;
+    private final static int REQUEST_CODE_GOOGLE_SIGN_IN = 1;
+    private final String TAG = getClass().getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +48,29 @@ public class RegistrationActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.text_input_edit_text_password);
         passwordTextInputLayout = findViewById(R.id.text_input_layout_password);
 
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
+
         auth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //TODO: Not sure if this check should be here or just in login activity. Would also need to check firebase auth and fb login.
+        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
+            Log.w(TAG, "User already signed in with google");
+            googleSignInClient.signOut(); // TODO: this is just for debugging
+        }
     }
 
     public void onRegisterButtonClick(android.view.View view) {
-        Toast.makeText(this, "TEST", Toast.LENGTH_LONG).show();
         // Validate email and set error message
         if (emailEditText.getText() == null) {
             return;
@@ -47,7 +78,7 @@ public class RegistrationActivity extends AppCompatActivity {
         Email email = new Email(emailEditText.getText().toString(), this);
         String errorMsg = email.getError();
         emailTextInputLayout.setError(errorMsg);
-        if (errorMsg == null) {
+        if (errorMsg != null) {
             return;
         }
 
@@ -58,7 +89,7 @@ public class RegistrationActivity extends AppCompatActivity {
         Password password = new Password(passwordEditText.getText().toString(), this);
         errorMsg = password.getError();
         passwordTextInputLayout.setError(errorMsg);
-        if (errorMsg == null) {
+        if (errorMsg != null) {
             return;
         }
 
@@ -67,12 +98,14 @@ public class RegistrationActivity extends AppCompatActivity {
                 .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegistrationActivity.this, "Account successfully created", Toast.LENGTH_LONG).show(); //TODO: remove
+                            Log.d(TAG, "Account successfully created");
+                        } else {
                             Exception exception = task.getException();
                             String msg = exception == null ? "" : ": " + exception.getLocalizedMessage();
-                            Toast.makeText(RegistrationActivity.this, msg, Toast.LENGTH_LONG).show();
-                        } else {
-                            Toast.makeText(RegistrationActivity.this, "CREATED ACCOUNT SUCCESS", Toast.LENGTH_LONG).show();
+                            Snackbar.make(coordinatorLayout, getString(R.string.auth_failed_registration) + msg, Snackbar.LENGTH_LONG).show();
+                            Log.d(TAG, getString(R.string.auth_failed_registration) + msg);
                         }
                     }
                 });
@@ -81,14 +114,56 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     private void onLoginButtonClick() {
-
+        finish();
     }
 
-    private void onRegisterGoogleButtonClick() {
-
+    // TODO: link google, fb, and email accounts to same user: https://firebase.google.com/docs/auth/android/account-linking?authuser=0
+    // TODO: move google sign in stuff to another file so login page can use it
+    public void onRegisterGoogleButtonClick(android.view.View view) {
+        Intent googleSignInClientSignInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(googleSignInClientSignInIntent, REQUEST_CODE_GOOGLE_SIGN_IN);
     }
 
     private void onRegisterFacebookButtonClick() {
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account == null) {
+                    Snackbar.make(coordinatorLayout, getString(R.string.failed_google_sign_in_message), Snackbar.LENGTH_LONG).show();
+                    Log.d(TAG, "null GoogleSignInAccount");
+                } else {
+                    authenticateWithGoogle(account);
+                }
+            } catch (ApiException e) {
+                Snackbar.make(coordinatorLayout, getString(R.string.failed_google_sign_in_message), Snackbar.LENGTH_LONG).show();
+                Log.d(TAG, e.getMessage());
+            }
+        }
+    }
+
+    private void authenticateWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegistrationActivity.this, "Google sign in successful", Toast.LENGTH_LONG).show(); //TODO: remove
+                            Log.d(TAG, "Google sign in successful");
+                        } else {
+                            Exception exception = task.getException();
+                            String msg = exception == null ? "" : ": " + exception.getLocalizedMessage();
+                            Snackbar.make(coordinatorLayout, getString(R.string.failed_google_sign_in_message) + msg, Snackbar.LENGTH_LONG).show();
+                            Log.d(TAG, getString(R.string.failed_google_sign_in_message) + msg);
+                        }
+                    }
+                });
     }
 }
