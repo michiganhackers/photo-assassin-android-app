74173fce9d82754764c4b6d0b7687d536a8be08d
tandunn <tandunn@umich.edu>
2019-04-11 20:09:18 -0400

    Login page progress

--- app/src/main/java/org/michiganhackers/photoassassin/LoginActivity.java
+++ app/src/main/java/org/michiganhackers/photoassassin/LoginActivity.java
@@ -2,6 +2,7 @@ package org.michiganhackers.photoassassin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -11,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onRegisterButtonClick(android.view.View view) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
