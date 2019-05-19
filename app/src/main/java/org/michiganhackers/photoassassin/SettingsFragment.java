b94a604a8960b7ae0a56be7fe83764f83731c7f8
rckhzouz <rckhzouz@umich.edu>
2019-05-19 02:06:05 -0400

    basic settings screen starting

---
+++ app/src/main/java/org/michiganhackers/photoassassin/SettingsFragment.java
@@ -0,0 +1,16 @@
+package org.michiganhackers.photoassassin;
+
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+}
+
+
+
+
+
