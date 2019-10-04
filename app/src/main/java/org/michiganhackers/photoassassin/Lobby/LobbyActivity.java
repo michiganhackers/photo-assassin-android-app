8dc1b6618750ef2e976ea3fe0d6baae1c3dea79a
ify <ezeifyyy@gmail.com>
2019-10-03 21:00:13 -0400

    initial commit

---
+++ app/src/main/java/org/michiganhackers/photoassassin/Lobby/LobbyActivity.java
@@ -0,0 +1,16 @@
+package org.michiganhackers.photoassassin.Lobby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.michiganhackers.photoassassin.R;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
