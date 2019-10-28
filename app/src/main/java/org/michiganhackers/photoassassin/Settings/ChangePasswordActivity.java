c0ff1a955041421d6943c4766220ae542a5354a4
rckhzouz <rckhzouz@umich.edu>
2019-10-27 23:05:47 -0400

    Added change email and change password designs

---
+++ app/src/main/java/org/michiganhackers/photoassassin/Settings/ChangePasswordActivity.java
@@ -0,0 +1,20 @@
+package org.michiganhackers.photoassassin.Settings;
+
+import android.os.Bundle;
+
+import org.michiganhackers.photoassassin.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_email);
+    }
+
+    public void onBackButtonClick(android.view.View view) {
+        finish();
+    }
+}
