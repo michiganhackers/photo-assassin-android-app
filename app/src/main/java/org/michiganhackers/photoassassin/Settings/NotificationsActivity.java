3df41044f5e03370aaac4a9a71f67541fde1f8c2
rckhzouz <rckhzouz@umich.edu>
2019-10-28 00:47:24 -0400

    Added notifications design

--- app/src/main/java/org/michiganhackers/photoassassin/Settings/NotificationsActivity.java
+++ app/src/main/java/org/michiganhackers/photoassassin/Settings/NotificationsActivity.java
@@ -7,11 +7,11 @@ import org.michiganhackers.photoassassin.R;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ChangePasswordActivity extends AppCompatActivity {
+public class NotificationsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_password);
+        setContentView(R.layout.activity_notifications);
     }
 
     public void onBackButtonClick(android.view.View view) {
