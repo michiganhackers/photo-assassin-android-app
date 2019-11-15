664147766b10170d3aee87f54e78b2b3396a8b14
David Wang <daviwang@umich.edu>
2019-11-14 21:25:03 -0500

    Attached important views to member variables, gave IDs to those views

--- app/src/main/java/org/michiganhackers/photoassassin/NewGame.java
+++ app/src/main/java/org/michiganhackers/photoassassin/NewGame.java
@@ -9,14 +9,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class NewGame extends AppCompatActivity {
+    private Button backButton;
+    private Spinner playerLimitSpinner;
+    private Button invitePlayersButton;
+    private Button createGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
+        // Attach views to member variables
+        backButton = (Button) findViewById(R.id.back_button);
+        playerLimitSpinner = (Spinner) findViewById(R.id.player_limit);
+        invitePlayersButton = (Button) findViewById(R.id.invite_players);
+        createGameButton = (Button) findViewById(R.id.create_game);
+
+        // Note: back button just calls finish() which pops it off the stack
+        // TODO: create listeners for buttons
+        // TODO: create and set adapter for spinner
     }
 
 }
